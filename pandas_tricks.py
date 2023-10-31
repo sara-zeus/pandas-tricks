2ffb1cf768a8413922fb8c626eed8feef79fd0f5
Sarooshka <74381399+sara-zeus@users.noreply.github.com>
2023-10-31 11:54:29 -0600

    Add files via upload

--- pandas_tricks.py
+++ pandas_tricks.py
@@ -1,23 +1,24 @@
 import pandas as pd
 
+
 def start():
-    options= {
-        "display":{
-            "max_columns":None, 
-            "Max_colwidth":25, 
-            "expand_frame_repr":False, 
-            "max_rows":14, 
-            "max_seq_items":50, 
-            "precision":4, 
-            "show_dimentions":False
-
-
-
-
-
-
-        }, 'mode':{ 
-            "chained_assignments": None
-        }
-
+    options = {
+        "display": {
+            "max_columns": None,
+            "max_colwidth": 25,
+            "expand_frame_repr": False,
+            "max_rows": 14,
+            "max_seq_items": 50,
+            "precision": 4,
+            "show_dimensions": False,
+        },
+        "mode": {"chained_assignment": None},
     }
+
+    for category, values in options.items():
+        for option, value in values.items():
+            pd.set_option(f"{category}.{option}", value)
+
+
+if __name__ == "__main__":
+    start()
