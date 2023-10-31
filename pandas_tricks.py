cf4d8ba77d84d5bab19eb129b813cb4d8b7be821
Sara Salehi <sarasalehi74@gmail.com>
2023-10-30 23:42:00 -0600

    Initial commit

---
+++ pandas_tricks.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def start():
+    options= {
+        "display":{
+            "max_columns":None, 
+            "Max_colwidth":25, 
+            "expand_frame_repr":False, 
+            "max_rows":14, 
+            "max_seq_items":50, 
+            "precision":4, 
+            "show_dimentions":False
+
+
+
+
+
+
+        }, 'mode':{ 
+            "chained_assignments": None
+        }
+
+    }
